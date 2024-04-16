fdb6916c00f9e7d7d1ef9fca9984c678308be892
JuanMiguel <juan.noriega@correounivalle.edu.co>
2024-04-16 13:17:38 -0500

    Se sube el proyecto de spring base

---
+++ src/main/java/com/pragma/users_microservice/UsersMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.users_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersMicroserviceApplication.class, args);
+	}
+
+}
